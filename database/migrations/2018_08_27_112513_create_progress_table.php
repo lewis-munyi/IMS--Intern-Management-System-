e4c544f3db65e1549ef136b8e2084a2cc09e4896
lewis-munyi <lewismunyi97@gmail.com>
2018-08-27 16:09:08 +0300

    Bug fixes

---
+++ database/migrations/2018_08_27_112513_create_progress_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProgressTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+//        Schema::table('progress', function (Blueprint $table) {
+//            // Rows
+//            $table->increments('id');
+//            $table->string('email')->unique();
+//            $table->string('week_1');
+//            $table->string('week_2');
+//            $table->string('week_3');
+//            $table->string('week_4');
+//            $table->string('week_5');
+//            $table->string('week_6');
+//            $table->string('week_7');
+//            $table->string('week_8');
+//            $table->string('week_9');
+//            $table->string('week_10');
+//            $table->string('week_11');
+//            $table->string('week_12');
+//            $table->string('status');
+//            $table->string('remark');
+//            $table->rememberToken();
+//            $table->timestamps();
+//        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+//        Schema::dropIfExists('progress');
+    }
+}
