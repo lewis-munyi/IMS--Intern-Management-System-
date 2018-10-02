0f3ad1d2b920eb68683932b6fc465aa77f7ac7c3
lewis-munyi <lewismunyi97@gmail.com>
2018-10-02 12:49:51 +0300

    Bug fixes and improvements

--- resources/views/school/dashboard.blade.php
+++ resources/views/school/dashboard.blade.php
@@ -3,11 +3,9 @@
 @section('form')
     <form method="POST" action="{{ route('login') }}">
         @csrf
-        <span>
             <p>
                 Enter your email address. All the details of the student you supervise will be sent to this email.
             </p>
-        </span>
         <div class="form-group">
             <label>Email address</label>
             <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Enter email" required autofocus>
